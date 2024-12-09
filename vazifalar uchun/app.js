9c8c564313d6f2e8bdad60c7a573ce808d12ce4f
Diyorbek282 <4415.diyor@gmail.com>
2024-12-09 15:42:59 +0500

    TV added

--- vazifalar uchun/app.js
+++ vazifalar uchun/app.js
@@ -2,17 +2,34 @@ const images = document.querySelectorAll("#images img");
 const pult = document.querySelectorAll("#pult");
 const lock_screen = document.getElementById("lock_screen");
 const top1 = document.getElementById("top");
+const bottom1 = document.getElementById("bottom");
+let img_sum = images.length;
 let counter = 0;
 
 const TV = () => {
   lock_screen.addEventListener("click", (e) => {
     if (counter === 0) {
-      counter = 1;
-      images[0].classList.remove("hidden");
+      images[counter    ].classList.remove("hidden");
+      counter++;
+      if (counter === 1) {
+        top1.addEventListener("click", () => {
+          images[0].classList.add("hidden");
+          images[counter].classList.remove("visible");
+          images[counter + 1].classList.add("visible");
+          counter++;
+        });
+      }
+      if (counter === 1) {
+        bottom1.addEventListener("click", () => {
+          images[counter].classList.remove("visible");
+          images[counter - 1].classList.add("visible");
+          counter--;
+        });
+      }
     } else {
-      counter = 0;
+      counter--;
       images.forEach((image) => {
-        image.classList.add("hidden");
+        image.classList.remove("visible");
       });
     }
   });
